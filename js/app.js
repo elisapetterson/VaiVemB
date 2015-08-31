fa80ba86a7a3bd197a0bf27c8ec614f3b1320e7e
Elisa Petterson <elisa@petterson.com>
2015-08-31 12:10:41 -0300

    angular

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
-(function(){
-var app = angular.module('vaivem', []);
-    
+(function () {
+    var app = angular.module("vaivem", []);
 
+    app.controller("mainCtrl", ['$scope', '$http', function ($scope, $http) {
+
+        this.selectedPage = 'splash';
+        
+        this.selectPage = function (newPage){
+
+            this.selectedPage = newPage;
+        };
+
+        this.isSelectedPage = function(selectPage){
+            return this.selectedPage === selectPage;
+        };
+
+    } ]);
     
 })();
